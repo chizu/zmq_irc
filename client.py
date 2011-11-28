04002ba07412c0573256a4184c797e0d0d347973
Nell Hardcastle <chizu@spicious.com>
2011-11-27 22:25:44 -0800

    Switch to twisted endpoint API and support SSL.

--- client.py
+++ client.py
@@ -7,10 +7,13 @@ from zmq.core import constants
 from txZMQ import ZmqFactory, ZmqEndpoint, ZmqConnection
 from twisted.enterprise import adbapi
 from twisted.internet import protocol, reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint, SSL4ClientEndpoint
+from twisted.internet.ssl import ClientContextFactory
 from twisted.words.protocols import irc
 
 
 dbpool = adbapi.ConnectionPool("psycopg2", database='hashi')
+zmqfactory = ZmqFactory()
 
 
 class ZmqPushConnection(ZmqConnection):
@@ -25,11 +28,11 @@ class ZmqPullConnection(ZmqConnection):
 
 
 class RemoteEventPublisher(object):
-    def __init__(self, zf, network, identity):
+    def __init__(self, network, identity):
         self.network = network
         self.identity = identity
         e = ZmqEndpoint("connect", "tcp://127.0.0.1:9911")
-        self.socket = ZmqPushConnection(zf, identity, e)
+        self.socket = ZmqPushConnection(zmqfactory, identity, e)
 
     def event(self, kind, *args):
         send = [self.network, self.identity, kind]
@@ -51,8 +54,7 @@ class Client(irc.IRCClient):
 
     def signedOn(self):
         self.channels = list()
-        zf = self.factory.hashi.zf
-        self.publish = RemoteEventPublisher(zf, self.network, self.nickname)
+        self.publish = RemoteEventPublisher(self.network, self.nickname)
         self.publish.event("signedOn", self.nickname)
         
     def joined(self, channel):
@@ -97,10 +99,13 @@ class HashiController(ZmqPullConnection):
         command = message[2:]
         subject = self.clients[user]
         # If there's a server specified, pick that server here
-        if server:
+        if server != "global":
             subject = subject[server]
         # Otherwise, the iterable subject means for all servers
-        pass
+        if command == 'connect':
+            pass
+        elif command == 'join':
+            pass
 
 
 class Hashi(object):
@@ -109,14 +114,14 @@ class Hashi(object):
         self.clients = defaultdict(list)
         self.config = json.load(open(config_path))
         e = ZmqEndpoint("connect", "tcp://127.0.0.1:9911")
-        self.zf = ZmqFactory()
-        self.socket = HashiController(self.zf, e, self.clients)
+        self.socket = HashiController(zmqfactory, e, self.clients)
+        self.ssl_context = ClientContextFactory()
 
     def start(self):
         """Initialize the IRC client.
 
         Load existing configuration and join all clients."""
-        start_sql = """SELECT user_email, hostname, port, nick
+        start_sql = """SELECT user_email, hostname, port, ssl, nick
 FROM servers JOIN server_configs ON (servers.id = server_configs.server_id)
 WHERE server_configs.enabled = true;
 """
@@ -125,9 +130,15 @@ WHERE server_configs.enabled = true;
         return d
 
     def server_init(self, servers_config):
-        for email, hostname, port, nick in servers_config:
-            client_f = ClientFactory(self, hostname, chan, nick)
-            d = reactor.connectTCP(hostname, port, client_f)
+        for email, hostname, port, ssl, nick in servers_config:
+            # We're reusing hostnames as network names for now
+            client_f = ClientFactory(hostname, nick)
+            if ssl:
+                point = SSL4ClientEndpoint(reactor, hostname, port,
+                                           self.ssl_context)
+            else:
+                point = TCP4ClientEndpoint(reactor, hostname, port)
+            d = point.connect(client_f)
             d.addCallback(self.register_client, email)
 
     def register_client(self, client, email):
